a6c528509c563b5bbad5694df8210512d637c5a3
Replit user <>
2023-10-16 01:12:53 +0000

    new try

--- heap.c
+++ heap.c
@@ -47,7 +47,7 @@ void heap_push(Heap* pq, void* data, int priority)
       if(pq->heapArray[parentIndex].priority < pq->heapArray[currentIndex].priority)
       {
         heapElem temp = pq->heapArray[currentIndex];
-        pq->heapArray[currentIdx] = pq->heapArray[parentIdx];
+        pq->heapArray[currentIndex] = pq->heapArray[parentIndex];
         pq->heapArray[parentIndex] = temp;
         currentIndex=parentIndex;
       }
